86c2e8daf3a1c8fc77a6d655e3131375c4a917f7
edwintantawi <edwintantawi.workspace@gmail.com>
2022-07-07 23:58:12 +0700

    refactor

---
+++ src/containers/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h1 className="text-center text-blue-500 font-bold text-2xl">
+      React personal notes
+    </h1>
+  );
+}
+
+export { App };
